463b45c9aa89f074437451166fb6c8b0fea498dd
Abhishek joshi <joshiabhishek2030@gmail.com>
2024-10-27 09:50:17 +0530

    repo cleanup

--- mongoDB/day04/backend/routes/loginRoute.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-const { getLogin, loginController } = require("../controllers/auth");
-const loginRouter = express.Router();
-loginRouter.get("/login", getLogin);
-loginRouter.post("/login", loginController);
-module.exports = loginRouter;
--- mongoDB/w26d2/routes/sampleAirbnb.js
+++
@@ -1,8 +0,0 @@
-const express = require("express")
-const router = express.Router()
-
-const getmovie  = require("../controllers/sampleAirbnb")
-
-router.get("/",getmovie)
-
-module.exports = router
